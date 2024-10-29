a4e50b800a28696f6ad428e00ffea9f1e9fdbec4
Anthony Irizarry <airiz04@outlook.com>
2024-10-28 21:28:34 -0400

    Initial Commit

---
+++ HabitHive/HabitHiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitHiveApp.swift
+//  HabitHive
+//
+//  Created by Anthony Irizarry on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitHiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
