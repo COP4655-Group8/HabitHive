5524f9960a3ed49582409dbe32bcabcfd9dd9777
Christopher <chrisfranco1403@gmail.com>
2024-11-12 12:55:32 -0500

    Able to implement full redeem page functionality with pop up included. Only thing that must be finzalized is the point system that is implemented through the whole app.

---
+++ HabitHive/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  HabitHive
+//
+//  Created by Chrisopher Franco on 11/5/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
---
+++ HabitHive/RedeemPageView.swift
@@ -0,0 +1,18 @@
+//
+//  RedeemPageView.swift
+//  HabitHive
+//
+//  Created by Chrisopher Franco on 10/31/24.
+//
+
+import SwiftUI
+
+struct RedeemPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RedeemPageView()
+}
