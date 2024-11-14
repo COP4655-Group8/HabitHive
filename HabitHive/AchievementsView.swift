89860d92a113e99b7749bd7f000da561f1c79963
Alejandro Martinez <alejandromart1031@gmail.com>
2024-11-13 21:19:28 -0500

    Created the achievements page with the days achivements done. Also added a tap function for each of the icons included so far. Tapping an icon will bring up a message for the user with details on how to get the acheivement.

---
+++ HabitHive/AchievementsView.swift
@@ -0,0 +1,18 @@
+//
+//  AchievementsView.swift
+//  HabitHive
+//
+//  Created by Alejandro Martinez on 10/30/24.
+//
+
+import SwiftUI
+
+struct AchievementsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AchievementsView()
+}
